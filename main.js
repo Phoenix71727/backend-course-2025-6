a56a230e6e298a8673eefd2aade724c5a202eba8
Павло bc2025-6 <motlukp@gmail.com>
2025-11-20 21:45:57 +0200

    Updated main.js which can start server and install superagent and nodemon

---
+++ main.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { program } = require("commander");
+
+program
+  .requiredOption("-h, --host <host>", "Server host")
+  .requiredOption("-p, --port <port>", "Server port")
+  .requiredOption("-c, --cache <path>", "Cache directory");
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!fs.existsSync(options.cache)) {
+    fs.mkdirSync(options.cache, { recursive: true });
+    console.log(`Created cache directory: ${options.cache}`);
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Webservice running\n");
+});
+
+server.listen(options.port, options.host, () => {
+    console.log(`Server running at http://${options.host}:${options.port}/`);
+});
